2f8b6017356ce5572d3a3a63bbbe019c0b226a5d
Daniel Viana <danielvianna43@gmail.com>
2026-04-18 02:02:02 -0300

    feat: atividade de busca

--- mongodb_node/scripts/index.js
+++ mongodb_node/scripts/index.js
@@ -2,29 +2,18 @@ import { client, connect } from "../db/conn.js";
 
 
 async function run() {
+    const db = await connect();
+
+    const usersCollection = db.collection("accounts");
+
+    const documentsToFind = { balance: { $gt: 3000 } };
+
     try {
-        const db = await connect();
 
-        const users = db.collection("accounts");
-
-        const accounts = [
-            {
-                account_id: 'MD123456789',
-                account_holder: 'Daniel Viana',
-                account_type: 'checking',
-                balance: 1971.50,
-            },
-            {
-                account_id: 'MD987654321',
-                account_holder: 'Renato Giorgi',
-                account_type: 'checking',
-                balance: 3500.00,
-            }
-        ]
-
-        const result = await users.insertMany(accounts);
-
-        console.log(result);
+        const result = usersCollection.find(documentsToFind)
+        const docCount = usersCollection.countDocuments(documentsToFind);
+        await result.forEach(doc => console.log(doc));
+        console.log('Total de documentos encontrados:', await docCount);
     } catch (error) {
         console.error("Erro ao inserir conta:", error);
     } finally {
