2290da0f496ecabb07194c290dd93eb92afcbd81
Daniel Viana <danielvianna43@gmail.com>
2026-04-18 01:47:50 -0300

    feat: atividade de inserção de multíplos documentos

--- mongodb_node/scripts/index.js
+++ mongodb_node/scripts/index.js
@@ -7,29 +7,22 @@ async function run() {
 
         const users = db.collection("accounts");
 
-        const accounts = {
-            account_id: 'MD123456789',
-            account_holder: 'Daniel Viana',
-            account_type: 'checking',
-            balance: 1971.50,
-            transfer_completed: [
-                "123456789",
-                "987654321",
-                "456789123",
-                "321654987",
-                "654321789"
-            ],
-            address: {
-                city: 'São Paulo',
-                state: 'SP',
-                zip_code: '12345-678',
-                country: 'Brazil',
-                number: 123,
-                street: 'Rua Exemplo'
+        const accounts = [
+            {
+                account_id: 'MD123456789',
+                account_holder: 'Daniel Viana',
+                account_type: 'checking',
+                balance: 1971.50,
+            },
+            {
+                account_id: 'MD987654321',
+                account_holder: 'Renato Giorgi',
+                account_type: 'checking',
+                balance: 3500.00,
             }
-        };
+        ]
 
-        const result = await users.insertOne(accounts);
+        const result = await users.insertMany(accounts);
 
         console.log(result);
     } catch (error) {
