8fddbafc749526b018c13cfafa9f113c363259b6
Daniel Viana <danielvianna43@gmail.com>
2026-04-18 15:05:36 -0300

    feat: atividade de exclusão de um documento

--- mongodb_node/scripts/index.js
+++ mongodb_node/scripts/index.js
@@ -7,15 +7,13 @@ async function run() {
 
     const usersCollection = db.collection("accounts");
 
-    const documentToUpdate = { account_type: 'checking' }
-
-    const update = { $push: { transfers_complete: "09876" } }
+    const documentToDelete = { _id: new ObjectId("69e30c604c3f45c75701d2c7") }
 
     try {
-        const result = await usersCollection.updateMany(documentToUpdate, update)
-        result.modifiedCount > 0
-            ? console.log(`Updated ${result.modifiedCount} documents`)
-            : console.log('No documents updated')
+        const result = await usersCollection.deleteOne(documentToUpdate)
+        result.deletedCount === 1
+            ? console.log("Deleted one document")
+            : console.log("No documents deleted");
     } catch (error) {
         console.error("Erro ao inserir conta:", error);
     } finally {
